65eb423f7f3cdbb1d0df08164a0e7b9bb69d942d
thurmanl <70459537+thurmanl@users.noreply.github.com>
2022-10-29 16:27:52 -0600

    finish bacon game

--- bacon/src/pages/Bacon.js
+++ bacon/src/pages/Bacon.js
@@ -16,54 +16,105 @@ class Bacon extends React.Component {
         "https://as1.ftcdn.net/v2/jpg/01/45/99/70/1000_F_145997055_yc0AXDgo36qmd32c87K0IDiX3qcCSUqH.jpg",
         "https://m.media-amazon.com/images/I/41E8Dbi2EjL._SR600%2C315_PIWhiteStrip%2CBottomLeft%2C0%2C35_PIStarRatingONE%2CBottomLeft%2C360%2C-6_SR600%2C315_SCLZZZZZZZ_FMpng_BG255%2C255%2C255.jpg"
       ], 
-      imgIndices:  [1, 2, 3, 4]
+      imgIndices:  [1, 2, 3, 4],
+      allowClick: true,
+      panClass: ["baconButton", "baconButton", "baconButton", "baconButton", "baconButton"]
     };
 
+    this.winBacon = this.winBacon.bind(this);
+    this.resetClass = this.resetClass.bind(this);
     this.buttonClick1 = this.buttonClick1.bind(this);
     this.buttonClick2 = this.buttonClick2.bind(this);
     this.buttonClick3 = this.buttonClick3.bind(this);
+    this.buttonClick4 = this.buttonClick4.bind(this);
+    this.buttonClick5 = this.buttonClick5.bind(this);
     this.randomizeImages = this.randomizeImages.bind(this);
+  }
+
+  resetClass(){
+    this.setState({ panClass: ["baconButton", "baconButton", "baconButton", "baconButton", "baconButton"] });
   }
 
   buttonClick1(event){
     event.preventDefault();
-    this.randomizeImages(0, 2);
+    if(this.state.panClass[0] === "baconButton"){
+      this.setState({ panClass: ["rotate", "baconButton", "baconButton", "baconButton", "baconButton"] });
+      setTimeout(this.resetClass, 1000);
+    }
+    this.randomizeImages([0, 1]);
   }
 
   buttonClick2(event) {
     event.preventDefault();
-    this.randomizeImages(1, 3);
+    if (this.state.panClass[1] === "baconButton") {
+      this.setState({ panClass: ["baconButton", "rotate", "baconButton", "baconButton", "baconButton"] });
+      setTimeout(this.resetClass, 1000);
+    }
+    this.randomizeImages([1, 2]);
   }
 
   buttonClick3(event) {
     event.preventDefault();
-    this.randomizeImages(2, 4);
+    if (this.state.panClass[2] === "baconButton") {
+      this.setState({ panClass: ["baconButton", "baconButton", "rotate", "baconButton", "baconButton"] });
+      setTimeout(this.resetClass, 1000);
+    }
+    this.randomizeImages([2, 3]);
   }
 
-  randomizeImages(start, stop){
+  buttonClick4(event) {
+    event.preventDefault();
+    if (this.state.panClass[3] === "baconButton") {
+      this.setState({ panClass: ["baconButton", "baconButton", "baconButton", "rotate", "baconButton"] });
+      setTimeout(this.resetClass, 1000);
+    }
+    this.randomizeImages([1, 3]);
+  }
+
+  buttonClick5(event) {
+    event.preventDefault();
+    if (this.state.panClass[4] === "baconButton") {
+      this.setState({ panClass: ["baconButton", "baconButton", "baconButton", "baconButton", "rotate"] });
+      setTimeout(this.resetClass, 1000);
+    }
+    this.randomizeImages([0, 2]);
+  }
+
+  randomizeImages(indices){
+    if(!this.state.allowClick){ return; }
+
     let newIndices = this.state.imgIndices;
-    for(let i = start; i < stop; i++){
+    indices.forEach(i => 
       newIndices[i] = Math.floor(Math.random() * this.state.notBacon.length)
-    }
-    var isTrue = newIndices.every(index => index == 0);
+    );
+    var isTrue = newIndices.every(index => index === 0);
     this.setState({imgIndices : newIndices});
+    
     if (isTrue) {
-      alert("you win!");
+      this.setState({allowClick: false});
+      setTimeout(this.winBacon, 1000);
     }
+  }
+
+  winBacon(){
+    alert("you win!");
   }
 
   render() {
     const notBacons = this.state.imgIndices.map((item, index) =>
-      <img key={index} src={this.state.notBacon[item]} width="300px" />
+      <img key={index} alt="not bacon" src={this.state.notBacon[item]} width="300px" />
     );
+    const panImage = "https://i.pinimg.com/originals/c3/6a/2d/c36a2da77aff809fc33fdf71d2585091.png";
     return (
-    <div id = "bodyDiv"><h1>Bacon for the win!</h1>
+    <div id = "bodyDiv"><h1>Flip the pans, Get 4 bacons to win!</h1>
     <div id = "baconImages">
           {notBacons}
     </div>
-    <button class="baconButton" onClick={this.buttonClick1}> Button 1</button>
-    <button class="baconButton" onClick={this.buttonClick2}> Button 2</button>
-    <button class="baconButton" onClick={this.buttonClick3}> Button 3</button>
+    <input type="image" alt="pan1" src={panImage} class={this.state.panClass[0]} onClick={this.buttonClick1} width="10%" />
+    <input type="image" alt="pan2" src={panImage} class={this.state.panClass[1]} onClick={this.buttonClick2} width="10%" />
+    <input type="image" alt="pan3" src={panImage} class={this.state.panClass[2]} onClick={this.buttonClick3} width="10%" />
+    <input type="image" alt="pan4" src={panImage} class={this.state.panClass[3]} onClick={this.buttonClick4} width="10%" />
+    <input type="image" alt="pan5" src={panImage} class={this.state.panClass[4]} onClick={this.buttonClick5} width="10%" />
     <footer>
           <p>Github Repository: <a href="https://github.com/pearlhulbert/CreativeProject3">https://github.com/pearlhulbert/CreativeProject3</a></p>
           <p>By: Noelle Marshall, Pearl Hulbert, Logan Thurman</p>
