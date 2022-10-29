91682926bbccc292826430553154aa19ae715556
Noelle Marshall <noelle.marshall17@gmail.com>
2022-10-29 21:02:13 +0000

    Added stuff to about

--- bacon/src/pages/About.js
+++ bacon/src/pages/About.js
@@ -1,9 +1,22 @@
 const About = () => {
-  return (<div id = "bodyDiv"><h1>About us</h1>
+  return (<div id = "bodyDiv"><h1>About Us</h1>
+      <div id="about-container">
+        <div class="abt-info">
+            <h2>Hi we are the creators!</h2>
+            <p>Our names are Pearl Hulbert, Logan Thurman, and Noelle Marshall. We are students at BYU and love bacon and we love webprogramming! And this is why we created this website for you. Well that and to make you want bacon, haha.
+            Is it working yet? </p>
+            </div>
+        <div class="abt-img">
+            <img src="https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/240395072_5151250331557064_7277126973944663761_n.jpg?stp=dst-jpg_p843x403&_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=HDMM5ZCHR7cAX97mFIi&_nc_ht=scontent-sea1-1.xx&oh=00_AfBe2-1cRFLpP-QsfczqVQ4taEaGVEPMRkyckoZ8GaX4Jg&oe=6362A9C0" width="33%"></img>
+            <img src="https://scontent-sea1-1.xx.fbcdn.net/v/t1.6435-9/145092149_449188186457901_183636433127160151_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=trZ8CEfEQAoAX_SCT4K&_nc_ht=scontent-sea1-1.xx&oh=00_AfBMtAyP9CH9iQQRFoZ9Yo2SncYvLIdtugCwW-LTfzK_KQ&oe=63832A59" width="33%"></img>
+            <img src="https://ec2-54-219-90-242.us-west-1.compute.amazonaws.com/photolab/pictures/DSC_8059.jpg" width="33%"></img>
+        </div>
+        
+      </div>
   <footer>
         <p>Github Repository: <a href="https://github.com/pearlhulbert/CreativeProject3">https://github.com/pearlhulbert/CreativeProject3</a></p>
         <p>By: Noelle Marshall, Pearl Hulbert, Logan Thurman</p>
-    </footer></ div>);
+    </footer></div>);
 };
 
 export default About;
