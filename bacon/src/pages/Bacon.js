05bfc604b342ff163cc1627adbcdff50ff02cf6f
Pearl Hulbert <pearl.k.hulbert@gmail.com>
2022-10-29 05:54:32 +0000

    bacon syntax?

--- bacon/src/pages/Bacon.js
+++ bacon/src/pages/Bacon.js
@@ -1,5 +1,42 @@
+import ReactDOM from "react-dom/client";
+import React from 'react';
+
 const Bacon = () => {
-  return (<div id = "bodyDiv"><h1>Bacon for the win!</h1>
+  return (
+  <div id = "bodyDiv"><h1>Bacon for the win!</h1>
+    <script type="text/babel">
+      class BaconGame extends React.Componenet {
+        constructor(props) {
+          super(props);
+          
+          this.handleChange = this.handleChange.bind(this);
+          
+        }
+        
+        handleChange(event) {
+          
+        }
+        
+        render() {
+          );
+        return(
+          <div>
+            <button onClick={this.handleChange}>Bacon</button>
+          </div>
+          );
+        }
+        
+        
+      }
+      const root = ReactDOM.createRoot(document.getElementById('root'));
+      root.render(<BaconGame />);
+      var url = "https://baconipsum.com/api/?type=meat-and-filler";
+        console.log("URL " + url);
+        fetch(url)
+          .then((data) => {
+            return (data.json());
+          })
+    </script>
   <footer>
         <p>Github Repository: <a href="https://github.com/pearlhulbert/CreativeProject3">https://github.com/pearlhulbert/CreativeProject3</a></p>
         <p>By: Noelle Marshall, Pearl Hulbert, Logan Thurman</p>
